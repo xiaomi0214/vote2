2f6683ada3269640a0765955e63c660b213b3a54
mixiongxiong <mixiongxiong@lixiang.com>
2023-10-14 17:45:15 +0800

    update result tests.sh

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -9,12 +9,12 @@ sleep 10
 
 if phantomjs render.js http://result | grep -q '1 vote'; then
   echo -e "\\e[42m------------"
-  echo -e "\\e[92mTests passed "
+  echo -e "\\e[92mTests passed"
   echo -e "\\e[42m------------"
   exit 0
 else
   echo -e "\\e[41m------------"
-  echo -e "\\e[91mTests failed "
+  echo -e "\\e[91mTests failed"
   echo -e "\\e[41m------------"
   exit 1
 fi
