d1c7e4ebfa6375709ef4e8ca0d639945a4381918
mixiongxiong <mixiongxiong@lixiang.com>
2023-10-14 18:07:40 +0800

    update vote app.py

--- vote/app.py
+++ vote/app.py
@@ -33,7 +33,7 @@ def hello():
     if request.method == 'POST':
         redis = get_redis()
         vote = request.form['vote']
-        app.logger.info('Received vote for %s ml', vote)
+        app.logger.info('Received vote for %s mll', vote)
         data = json.dumps({'voter_id': voter_id, 'vote': vote})
         redis.rpush('votes', data)
 
